b19e97e8b2d8178796352a7f8e01eeb003f2a10c
Stephen Vickers <stephen@spvsoftwareproducts.com>
2026-09-09 18:15:28 +0100

    Update version property

--- src/Util.php
+++ src/Util.php
@@ -176,7 +176,7 @@ final class Util
      *
      * @var string $version
      */
-    public static string $version = '5.4.4';
+    public static string $version = '5.4.5';
 
     /**
      * Value of User-Agent header of HTTP requests.
