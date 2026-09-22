a20b37ccb64c53d6966a39bcf3a9335702a0d284
Stephen Vickers <stephen@spvsoftwareproducts.com>
2026-09-22 23:39:17 +0100

    Load tool based on client ID when receiving an authentication request

--- src/Platform.php
+++ src/Platform.php
@@ -991,13 +991,17 @@ EOD;
             $this->ok = false;
             $this->messageParameters['error'] = 'unauthorized_client';
         }
-        if ($this->ok && empty(Tool::$defaultTool)) {
+        $tool = Tool::$defaultTool;
+        if ($this->ok && empty($tool)) {
+            $tool = Tool::fromConsumerKey($this->clientId, $platform->getDataConnector());
+        }
+        if ($this->ok && !$tool->enabled) {
             $this->ok = false;
             $this->messageParameters['error'] = 'server_error';
-            $this->messageParameters['error_description'] = 'Tool not configured by platform';
+            $this->messageParameters['error_description'] = 'Tool not found or enabled';
         }
         if ($this->ok) {
-            $this->ok = in_array($parameters['redirect_uri'], Tool::$defaultTool->redirectionUris);
+            $this->ok = in_array($parameters['redirect_uri'], $tool->redirectionUris);
             if (!$this->ok) {
                 $this->messageParameters['error'] = 'invalid_request';
                 $this->messageParameters['error_description'] = 'Unregistered redirect_uri';
